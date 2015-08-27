8bfefcd0e0f08aee37a4c2522bcd53f93029e873
henrycm <henrycm@gmail.com>
2015-08-27 17:03:25 -0400

    Fix service return types

--- src/main/java/com/jhcm/rest/user/JSONService.java
+++ src/main/java/com/jhcm/rest/user/JSONService.java
@@ -13,7 +13,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
@@ -25,36 +24,36 @@ public class JSONService {
 
 	@GET
 	public Collection<User> get() {
+		System.out.println("GET:");
 		return users.values();
 	}
 
 	@GET
 	@Path("{id}")
 	public User get(@PathParam("id") int id) {
+		System.out.println("GET:" + id);
 		return users.get(id);
 	}
 
 	@POST
-	public Response create(User user) {
+	public User create(User user) {
 		user.setId(++id_seq);
+		System.out.println("POST:" + user.getName());
 		users.put(user.getId(), user);
-		String result = "User saved : " + user;
-		return Response.status(201).entity(result).build();
-
+		return users.get(user.getId());
 	}
 
 	@PUT
 	@Path("{id}")
-	public Response update(User user) {
-		users.put(user.getId(), user);
-		String result = "User saved : " + user;
-		return Response.status(201).entity(result).build();
-
+	public User update(User user) {
+		System.out.println("PUT:" + user.getId());
+		return users.put(user.getId(), user);
 	}
 
 	@DELETE
 	@Path("{id}")
 	public void remove(@PathParam("id") int id) {
+		System.out.println("DELETE:");
 		users.remove(id);
 	}
 
