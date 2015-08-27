4de8f1703d4aef3338876d98e4a043226527969e
johecama <henrycm@gmail.com>
2015-08-27 19:35:28 -0400

    Ajax error handler

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -4,3 +4,7 @@ app.utils.loadTemplates([ "UserView", "UserListView", "UserListItemView" ],
 		function() {
 			new app.AppView;
 		});
+
+$(document).ajaxError(function(event, xhr, options, exc) {
+	console.error("ajaxError:" + exc);
+});
